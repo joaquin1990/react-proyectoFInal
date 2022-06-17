f156e9763676a4d63cf72eb6127f990f1dd2e917
joaquin1990 <joaquingarese@gmail.com>
2022-06-17 10:14:16 -0300

    Color adjustment

--- src/otros/notas.js
+++ src/otros/notas.js
@@ -1,15 +1,1 @@
-// - Take away all the things that are not used in the code.
-// - Delete all the commented code.
-// - Organice the imports in the right order. Comments on 10:55 mins
-// - Add without stock if any product has 0 stock.
-// - Move the button "Vaciar Carrito" to the cart.
 // - Add readme.md to the project, to explain what is the project, like libraries used, etc, add a link to the project in the readme.md.
-// - Add a browser to search for products, and make the enter key function as a search button. In this form, in the email field, there have to be two fields, and the user would have to enter the email twice, and if they are the same, then a button to end the purchase would be enabled.
-
-// - Dudas pipe Hoy:
-
-// - Ayudas:
-// - To generate an object with all the values
-// - Para generar un objeto con todos los values del input (https://www.youtube.com/watch?v=tIdNeoHniEY) min 12:00.
-// -
-//
